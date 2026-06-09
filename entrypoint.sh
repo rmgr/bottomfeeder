5ed26c6d4472c3298e4e32342c94d01ffda11c89
jake <jake@rmgr.dev>
2026-06-09 21:00:44 +0930

    Fix broken dockerfile entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 
 uv run alembic upgrade head
-PYTHONPATH=src exec .venv/bin/uvicorn main:app --proxy-headers --forwarded-allow-ips="*" --port 8000 --host 0.0.0.0
+cd src && PYTHONPATH=. exec ../.venv/bin/uvicorn main:app --proxy-headers --forwarded-allow-ips="*" --port 8000 --host 0.0.0.0
